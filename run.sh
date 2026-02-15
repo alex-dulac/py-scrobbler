eb2021bd0c2d67f0a177221e848d6da869fedc6c
Alex Dulac <adulac29@gmail.com>
2026-02-15 14:52:32 -0500

    run.sh

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "venv" ]; then
+    echo "Virtual environment not found."
+    read -p "Would you like to create and install it? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "Creating virtual environment..."
+        python -m venv venv
+        echo "Installing dependencies..."
+        python -m pip install --upgrade pip
+        source venv/bin/activate
+        pip install -r requirements.txt
+    else
+        echo "Exiting."
+        exit 1
+    fi
+else
+    echo "Activating virtual environment..."
+    source venv/bin/activate
+fi
+
+echo "Starting textual app..."
+python textual_app.py
